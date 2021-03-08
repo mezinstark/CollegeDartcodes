3935de6106b0648c3bda0dfa3a5a39198541a1ad
mezinstark <mezin4nasir@gmail.com>
2021-03-08 11:14:35 +0530

    adding Q02

---
+++ smplarithmetic.dart
@@ -0,0 +1,12 @@
+void main(){
+  int var01=10;
+  int var02=20;
+  int sum=var01+var02;
+  int diff= var02-var01;
+  int product=var01*var02;
+  int div=var02/var01;
+  print("Sum=");print(sum);
+  print("Difference="); print(diff);
+  print("product="); print(product);
+  print("Division="); print(div);
+}
